2891a478b6af5f55bbe4aa0f4da4e8c8f3dfc481
Heike Kasper <heike.kasper@kadelo.de>
2022-10-20 16:17:00 +0200

    add post and get endpoint for products

--- miniatur_backend/src/app.controller.ts
+++ miniatur_backend/src/app.controller.ts
@@ -9,12 +9,13 @@ import {
   Query,
 } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
-import { User as UserModel, Prisma } from '@prisma/client'
+import { User as UserModel, Product as ProductModel, Prisma } from '@prisma/client'
 
 @Controller()
 export class AppController {
   constructor(private readonly prismaService: PrismaService) {}
 
+//User Endpoints
   @Get('users')
   async getAllUsers(
     @Query('take') take?: number,
@@ -39,8 +40,7 @@ export class AppController {
       last_name: string
       email: string
       password: string
-      user_name: string
-      
+      user_name: string   
     },
   ): Promise<UserModel> {
     
@@ -54,6 +54,7 @@ export class AppController {
       },
     })
   }
+
   @Put('user/:id')
  async UpdateUser(@Param('id') id: string ,@Body()userData: {
       first_name: string
@@ -75,13 +76,58 @@ export class AppController {
         user_name: userData.user_name,
         image: userData.image,
         about: userData.about
-
-        }
-      
+        } 
     })
  }
+
  @Delete('user/:id')
  async DeleteUser(@Param ('id') id: String): Promise<UserModel>{
   return this.prismaService.user.delete({where: {id: Number(id)}})
  }
+
+//Product Endpoint
+@Get('products')
+async GetAllProducts(
+    @Query('take') take?: number,
+    @Query('skip') skip?: number,
+    @Query('searchString') searchString?: string,
+    @Query('orderBy') orderBy?: 'asc' | 'desc',
+    ): Promise<ProductModel[]>{
+      const or = searchString ? {
+      OR: [
+        { name: { contains: searchString } },
+        { brand: { contains: searchString } },
+        {color:{contains: searchString}}
+      ],} : {}
+      return this.prismaService.product.findMany({
+      where: {...or},
+      take: Number(take) || undefined,
+      skip: Number(skip) || undefined,
+      orderBy: {
+        name: orderBy,
+        brand: orderBy,
+        color: orderBy
+      }
+    })
+}
+@Post('product')
+async CreateProduct(@Body() productData:{
+  categorie: string  
+	brand:string       
+	name:string       
+	color:string       
+	description: string
+	link: string       
+}): Promise<ProductModel>{
+  return this.prismaService.product.create({
+      data: {
+        categorie: productData.categorie,
+        brand: productData.brand,
+        name: productData.name,
+        color: productData.color,
+        description: productData.description,
+        link: productData.link
+      }
+    })
+}
 }
